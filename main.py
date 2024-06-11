3cf5fdf6aff101065bc24d056d87841643064024
masudnabil35 <masudnabil35@gmail.com>
2024-06-11 18:07:50 +0000

    1

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.Random import get_random_bytes
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+import time
 
 app = Flask(__name__)
 
@@ -25,10 +26,13 @@ def index():
 def encrypt():
     algorithm = request.form['algorithm']
     message = request.form['message'].encode()
+    encryption_time = None
 
     if algorithm == 'rsa':
         rsa_cipher = PKCS1_OAEP.new(RSA.import_key(rsa_public_key))
+        start_time = time.time()
         encrypted_message = rsa_cipher.encrypt(message)
+        encryption_time = time.time() - start_time
     elif algorithm == 'ecc':
         shared_key = ecc_private_key.exchange(ec.ECDH(), ecc_public_key)
         derived_key = HKDF(
@@ -37,18 +41,23 @@ def encrypt():
             salt=None,
             info=b'handshake data'
         ).derive(shared_key)
+        start_time = time.time()
         encrypted_message = derived_key + message  # Simplified encryption
+        encryption_time = time.time() - start_time
 
-    return render_template('index.html', encrypted_message=encrypted_message.hex(), algorithm=algorithm)
+    return render_template('index.html', encrypted_message=encrypted_message.hex(), encryption_time=encryption_time, algorithm=algorithm)
 
 @app.route('/decrypt', methods=['POST'])
 def decrypt():
     algorithm = request.form['algorithm']
     encrypted_message = bytes.fromhex(request.form['encrypted_message'])
+    decryption_time = None
 
     if algorithm == 'rsa':
         rsa_cipher = PKCS1_OAEP.new(RSA.import_key(rsa_private_key))
+        start_time = time.time()
         decrypted_message = rsa_cipher.decrypt(encrypted_message)
+        decryption_time = time.time() - start_time
     elif algorithm == 'ecc':
         shared_key = ecc_private_key.exchange(ec.ECDH(), ecc_public_key)
         derived_key = HKDF(
@@ -57,9 +66,11 @@ def decrypt():
             salt=None,
             info=b'handshake data'
         ).derive(shared_key)
+        start_time = time.time()
         decrypted_message = encrypted_message[len(derived_key):]  # Simplified decryption
+        decryption_time = time.time() - start_time
 
-    return render_template('index.html', decrypted_message=decrypted_message.decode(), algorithm=algorithm)
+    return render_template('index.html', decrypted_message=decrypted_message.decode(), decryption_time=decryption_time, algorithm=algorithm)
 
 if __name__ == '__main__':
     app.run(debug=True)
